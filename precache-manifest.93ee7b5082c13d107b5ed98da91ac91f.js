0d7aaf60e2a7498fe8049fc5bae887cee3f57c0b
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-08-21 00:42:42 -0700

    Updates

---
+++ precache-manifest.93ee7b5082c13d107b5ed98da91ac91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e46590a5bdb79579abd05dfdd8acf59",
+    "url": "/todo_list_redux_updated/index.html"
+  },
+  {
+    "revision": "5d26a8b392d4c02bc147",
+    "url": "/todo_list_redux_updated/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86e70e9768af5b003dea",
+    "url": "/todo_list_redux_updated/static/js/2.fd82d119.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_list_redux_updated/static/js/2.fd82d119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d26a8b392d4c02bc147",
+    "url": "/todo_list_redux_updated/static/js/main.b5f05fc0.chunk.js"
+  },
+  {
+    "revision": "3e81de54e73264a021fa",
+    "url": "/todo_list_redux_updated/static/js/runtime-main.5ab691f7.js"
+  }
+]);
